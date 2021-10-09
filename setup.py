376cfcf02d12c2c5a1fa8f8f2b14c6f8f7134f09
Ankit Mani <55287567+Ankitmani151@users.noreply.github.com>
2021-10-09 14:02:32 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open("README.md", "r", encoding="utf-8") as f:
 setup(
     name="src",
     version="0.0.1",
-    author="c17hawke",
+    author="Ankitmani151",
     description="A small package for ANN Implementation",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/c17hawke/ANN-implementation-DLCVNLP-demo",
-    author_email="sunny.c17hawke@gmail.com",
+    url="https://github.com/Ankitmani151/ANN-implementation",
+    author_email="ankitmani151@gmail.com",
     packages=["src"],
     python_requires=">=3.7",
     install_requires=[
