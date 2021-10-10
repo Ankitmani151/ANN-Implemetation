35af72a5eddf14e06d82e8cb23311723de0fa031
Ankit Mani <55287567+Ankitmani151@users.noreply.github.com>
2021-10-10 11:59:37 +0530

    Update training.py

--- src/training.py
+++ src/training.py
@@ -23,6 +23,16 @@ def training(config_path):
     history = model.fit(X_train, y_train, epochs=EPOCHS,
                     validation_data=VALIDATION_SET)
 
+    artifacts_dir = config["artifacts"]["artifacts_dir"]
+    model_dir = config["artifacts"]["model_dir"]
+    
+    model_dir_path = os.path.join(artifacts_dir, model_dir)
+    os.makedirs(model_dir_path, exist_ok=True)
+    
+    model_name = config["artifacts"]["model_name"]
+
+    save_model(model, model_name, model_dir_path)
+
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
 
