5d9cc1393731b39ac7bc3bd53e11f4a0d5c70a35
Ankit Mani <55287567+Ankitmani151@users.noreply.github.com>
2021-10-10 12:09:06 +0530

    Update training.py

--- src/training.py
+++ src/training.py
@@ -1,6 +1,7 @@
+import os
 from src.utils.common import read_config
 from src.utils.data_mgmt import get_data
-from src.utils.model import create_model
+from src.utils.model import create_model, save_model
 import argparse
 
 def training(config_path):
